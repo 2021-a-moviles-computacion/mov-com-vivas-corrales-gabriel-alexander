10f02f6df46652780113bca7d1f80492539b58a6
Gabriel Vivas <gabriel.vivas@epn.edu.ec>
2021-06-16 17:49:21 -0500

    Líneas de Crear archivo, escribir, leer, actualizar y eliminar archivo.

---
+++ Deber01/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import java.io.File
+import java.io.PrintWriter
+
+fun main(){
+    val fileName = "data.txt"
+    //var file = File(fileName)
+    //if (file.createNewFile()) println("$fileName se ha creado correctamente") else println("$fileName  ya existe")
+
+    //val content = "Hello World. Welcome to Kotlin Tutorial by www.tutorialkart.com."
+    //val writer = PrintWriter(fileName)
+    //writer.append(content)
+    //writer.close()
+
+    //val content:String = " This is additional content added to the File."
+    //File(fileName).appendText(content)
+
+    //File(fileName).delete()
+}
