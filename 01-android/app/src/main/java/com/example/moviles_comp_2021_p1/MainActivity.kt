837a2e831f9c0d8041cf82a208f7ff69039352e2
Gabriel Vivas <gabriel.vivas@epn.edu.ec>
2021-06-25 01:10:00 -0500

    Clase 23-06-2021

--- 01-android/app/src/main/java/com/example/moviles_comp_2021_p1/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/moviles_comp_2021_p1/MainActivity.kt
@@ -9,18 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val botonIrACicloVida = findViewById<Button>(
-            R.id.btn_ciclo_vida
-        )
-        botonIrACicloVida.setOnClickListener {
-            abrirCicloVida() }
+        val botonIrACicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        val botonIrListView = findViewById<Button>(R.id.btn_ir_list_view)
+        botonIrACicloVida.setOnClickListener { abrirActividad(ACicloVida::class.java) }
+        botonIrListView.setOnClickListener { abrirActividad(BListView::class.java) }
     }
 
-    fun abrirCicloVida(){
+    fun abrirActividad(clase: Class<*>){
                                 //clase
         val intentExplicito = Intent(
             this,
-            ACicloVida::class.java
+            clase
         )
         //puedo poner this opcional this.startActivity
         startActivity(intentExplicito)
