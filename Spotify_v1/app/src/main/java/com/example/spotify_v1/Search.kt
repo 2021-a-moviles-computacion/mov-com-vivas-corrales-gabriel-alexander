0b1a631fef9781650abdd7bad3ad0c40bd5d5757
Gabriel Vivas <gabriel.vivas@epn.edu.ec>
2021-08-13 18:26:59 -0500

    Home,Search,Library Completos

--- Spotify_v1/app/src/main/java/com/example/spotify_v1/Search.kt
+++ Spotify_v1/app/src/main/java/com/example/spotify_v1/Search.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
+var recyclerTopGenres : RecyclerView? = null
+var recyclerBrowseAll : RecyclerView? = null
 /**
  * A simple [Fragment] subclass.
  * Use the [Search.newInstance] factory method to
@@ -33,8 +36,39 @@ class Search : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val fragmentSearch = inflater.inflate(R.layout.fragment_search, container, false)
+        recyclerTopGenres = fragmentSearch.findViewById(R.id.top_genres_recycler)
+        recyclerBrowseAll = fragmentSearch.findViewById(R.id.browse_all_recycler)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return fragmentSearch
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val listaItemsTopGenres = arrayListOf<Cancion>()
+        listaItemsTopGenres.add(Cancion("hola","IMAGEN"))
+        listaItemsTopGenres.add(Cancion("hola","IMAGEN"))
+        listaItemsTopGenres.add(Cancion("hola","IMAGEN"))
+        listaItemsTopGenres.add(Cancion("hola","IMAGEN"))
+
+        val listaItemsBrowseAll = arrayListOf<Cancion>()
+        for (i in 1..60) {
+            listaItemsBrowseAll.add(Cancion("hola","IMAGEN"))
+        }
+
+        //DEBO CREAR UNA LISTA PARA CADA RECYCLER
+        iniciarRecyclerViewWelcome(listaItemsTopGenres,this, recyclerTopGenres!!,AdaptadorSearch(this,listaItemsTopGenres, recyclerTopGenres!!))
+        iniciarRecyclerViewWelcome(listaItemsBrowseAll,this, recyclerBrowseAll!!,AdaptadorSearch(this,listaItemsBrowseAll, recyclerBrowseAll!!))
+    }
+
+    fun iniciarRecyclerViewWelcome(lista: List<*>, actividad: Search, recyclerView: RecyclerView, adaptador:RecyclerView.Adapter<*>){
+        recyclerView.adapter = adaptador
+        recyclerView.itemAnimator= androidx.recyclerview.widget.DefaultItemAnimator()
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        // recyclerView.layoutManager = LinearLayoutManager(actividad.context, LinearLayoutManager.HORIZONTAL, false)
+        adaptador.notifyDataSetChanged()
     }
 
     companion object {
